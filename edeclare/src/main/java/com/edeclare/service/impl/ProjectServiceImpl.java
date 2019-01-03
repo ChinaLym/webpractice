53d0bd7e33b0987c017263bb88945dcbae209be7
Star_LYM <940723217@qq.com>
2019-01-03 22:46:22 +0800

    处理警告

--- edeclare/src/main/java/com/edeclare/service/impl/ProjectServiceImpl.java
+++ edeclare/src/main/java/com/edeclare/service/impl/ProjectServiceImpl.java
@@ -1,7 +1,6 @@
 package com.edeclare.service.impl;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
